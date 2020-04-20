e5bec1b624f41fb5d33b75890247ce17a7e6e6bb
RedCAT <akaineko18@gmail.com>
2020-04-20 17:46:52 +1200

    section and presenter a bit

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,38 @@
-export default () => 'Home';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import Section from 'Components/Section';
+
+const Container = styled.div`
+  padding: 0 10px;
+`;
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map((movie) => movie.title)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming">
+          {upcoming.map((movie) => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular">{popular.map((movie) => movie.title)}</Section>
+      )}
+    </Container>
+  );
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  upcoming: PropTypes.array,
+  popular: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default HomePresenter;
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,24 @@
-export default () => 'Search';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const SearchPresenter = ({
+  movieResults,
+  showResults,
+  searchTerm,
+  error,
+  loading,
+}) => {
+  return null;
+};
+
+SearchPresenter.propTypes = {
+  movieResults: PropTypes.array,
+  showResults: PropTypes.array,
+  searchTerm: PropTypes.string,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export default SearchPresenter;
