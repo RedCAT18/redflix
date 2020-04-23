f4699c82df3c23021b43d2f4c7e5dc45b4059e5c
RedCAT <akaineko18@gmail.com>
2020-04-23 15:14:54 +1200

    Updates

---
+++ precache-manifest.a6bad4679a2731ca1755a29145d97c4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d7cece315274cc4551e28de8b8550e",
+    "url": "/redflix/index.html"
+  },
+  {
+    "revision": "2b27fb20c3cd2c17ef97",
+    "url": "/redflix/static/js/2.45a05fca.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/redflix/static/js/2.45a05fca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d40459d081ea79400c72",
+    "url": "/redflix/static/js/main.101d2724.chunk.js"
+  },
+  {
+    "revision": "0240c6f0f6d33aaa9355",
+    "url": "/redflix/static/js/runtime-main.96f5260b.js"
+  },
+  {
+    "revision": "1217af4694feb8bab3094319eccc0e10",
+    "url": "/redflix/static/media/noPosterBig.1217af46.png"
+  },
+  {
+    "revision": "f38d1ff78f6fd30ff07b4d88eeba440e",
+    "url": "/redflix/static/media/noPosterSmall.f38d1ff7.png"
+  }
+]);
